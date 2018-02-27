f4e4c4c3d0ad3865e4e6f022808d5c8c981faf36
gasolin <gasolin@gmail.com>
2018-02-27 15:32:34 +0800

    💄: use bootstrap theme

--- src/components/Blocks.js
+++ src/components/Blocks.js
@@ -1,20 +1,39 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {
+  Badge,
+  // Button,
+  Table,
+  Navbar,
+  NavbarBrand,
+} from 'reactstrap';
+import {
+  clear,
   getBlocks,
   loadBlocks,
-  clear
 } from '../actions';
 
 export function Blocks(props) {
   return (
     <div>
-      <h3>Latest Block #{props.latestBlock}</h3>
-      <button type="button" onClick={props.getBlocks} >Get Blocks</button>
-      <button type="button" onClick={props.loadBlocks} >Load Demo Blocks</button>
-      <button type="button" onClick={props.clear} >Clear</button>
+      <Header {...props} />
+      {/* <h3>Latest Block #{props.latestBlock}</h3>
+      <Button outline color="primary" onClick={props.getBlocks} >Get Blocks</Button>{' '}
+      <Button outline onClick={props.loadBlocks} >Load Demo Blocks</Button>{' '}
+      <Button outline onClick={props.clear} >Clear</Button> */}
       <BlockList {...props} />
     </div>
+  );
+}
+
+function Header(props) {
+  let latestBlock = props.latestBlock ? (
+    <Badge color="info" tooltip="Latest Block">#{props.latestBlock}</Badge>
+  ): "";
+  return (
+    <Navbar color="dark" dark expand="lg">
+      <NavbarBrand href="#transactions">Transactions Monitor {latestBlock}</NavbarBrand>
+    </Navbar>
   );
 }
 
@@ -27,16 +46,29 @@ function BlockList(props) {
 
   if (props.blocks.length === 0) return null;
   return (
-    <div>
-      {props.blocks.sort((a, b) => b.number - a.number).map(item => <Block {...item} key={item.number} />)}
-    </div>
+    <Table>
+      <thead>
+        <tr>
+          <th>Block #</th>
+          <th>Transactions with value</th>
+          <th>Transactions</th>
+        </tr>
+      </thead>
+      <tbody>
+      {props.blocks.sort((a, b) => b.number - a.number).map(block =>
+        <Block {...block} key={block.number}/>)}
+      </tbody>
+    </Table>
   );
 }
 
-function Block(props) {
-  return (<div><h3>#{props.number}</h3>
-    <span>{props.valueTransactions.length}/{props.totalTransactionsLength}</span>
-  </div>);
+function Block(block) {
+  return (<tr>
+    <th>{block.number}</th>
+    <th><a href="#transactions" id={block.number}>{block.valueTransactions.length}</a></th>
+    <th>{block.totalTransactionsLength}</th>
+    <th>{new Date(block.timestamp * 1000).toLocaleString()}</th>
+  </tr>);
 }
 
 function mapState(state) {
@@ -44,14 +76,15 @@ function mapState(state) {
     blocks: state.blocks.blocks,
     latestBlock: state.blocks.latestBlock,
     loading: state.blocks.loading,
+    selectedBlock: state.blocks.selectedBlock
   };
 }
 
 function mapDispatch(dispatch) {
   return {
+    clear: () => dispatch(clear()),
     getBlocks: () => dispatch(getBlocks()),
     loadBlocks: () => dispatch(loadBlocks()),
-    clear: () => dispatch(clear())
   }
 }
 
